31f85693082617e063dbdd3d7f9142827e0f45df
mhgbrown <brown.mhg@gmail.com>
2015-05-16 14:38:02 -0500

    Fix deletion of messages after tweeting
    * key was to use id_str

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 var Twit = require('twit'),
   OARequest = require('twit/lib/oarequest'),
   opts = require('commander');
+
   opts.version('0.0.1')
   .option('-v, --verbose', 'Log some debug info to the console')
   .parse(process.argv);
 
 // Initialize Twitter API keys
 var twitter = new Twit({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
 
 // Verify the credentials
 twitter.get('/account/verify_credentials', function(error, data) {
@@ -23,7 +24,7 @@ twitter.get('/account/verify_credentials', function(error, data) {
   }
 
   if(opts.verbose) {
-    console.log(JSON.stringify(data));
+    console.log("credential verification: " + JSON.stringify(data));
   }
 
 });
@@ -34,7 +35,7 @@ stream.on('direct_message', function(directMessage) {
   var currentMessage = directMessage.direct_message;
 
   if(opts.verbose) {
-    console.log("credential verification: " + JSON.stringify(currentMessage));
+    console.log(JSON.stringify(currentMessage));
   }
 
   // bail if there is no direct message to check out
@@ -56,25 +57,22 @@ stream.on('direct_message', function(directMessage) {
 
   });
 
-  // direct messages destroy endpoint uses a weird schema so we have to
-  // "do it live"
-  new OARequest(
-    twitter.auth,
-    'POST',
-    'https://api.twitter.com/1.1/direct_messages/destroy.json?id=' + currentMessage.id,
-    {}
-  ).end(function(error, data) {
+  // delete the message
+  // NOTE: funny that id_str is the actual id of the message
+  twitter.post('direct_messages/destroy', { id: currentMessage.id_str },  function(error, data) {
 
-    // ruh roh
-    // TODO susceptible to rate limits
     if(error) {
-      console.error("destroy message " + currentMessage.id + ": " + JSON.stringify(error));
+      console.error("message destroy: " + JSON.stringify(error));
+    }
+
+    if(opts.verbose) {
+      console.log(JSON.stringify(data));
     }
 
   });
 });
 
-stream.on('warning', function(warning){
+stream.on('warning', function(warning) {
   console.warn("stream warning: " + JSON.stringify(warning));
 });
 
@@ -83,10 +81,10 @@ stream.on('disconnect', function(disconnect) {
 });
 
 // Handle exit signals
-process.on('SIGINT', function(){
+process.on('SIGINT', function() {
   process.exit(1);
 });
 
-process.on('exit', function(){
+process.on('exit', function() {
   console.log("Exiting...");
 });
